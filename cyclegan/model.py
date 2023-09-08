6789907bb42496f0a094d3f17f3f24160d20f683
Mohammadreza Salehi <mohammadreza.salehi951@gmail.com>
2023-09-08 14:32:10 -0700

    wandb:disabled

--- cyclegan/model.py
+++ cyclegan/model.py
@@ -1,4 +1,4 @@
-from cyclegan.layers import GeneralConv2D, GeneralDeconv2D
+from cyclegan.layers import *
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -17,7 +17,60 @@ IMG_HEIGHT = 256
 IMG_WIDTH = 256
 ngf = 32
 ndf = 64
+class ResnetBlock(nn.Module):
+    def __init__(self, dim, padding_type='reflect', norm_type=None, use_dropout=True, dropout_rate=0.25):
+        super(ResnetBlock, self).__init__()
+        self.conv_block = self.build_conv_block(dim, padding_type, norm_type, use_dropout, dropout_rate)
 
+    def build_conv_block(self, dim, padding_type, norm_type, use_dropout, dropout_rate):
+        conv_block = []
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        else:
+            p = 1
+
+        conv_block += [GeneralConv2d(dim, dim, kernel_size=3, stride=1, padding=p),
+                       nn.ReLU(True)]
+        
+        if use_dropout:
+            conv_block += [nn.Dropout(dropout_rate)]
+        
+        p = 0
+        if padding_type == 'reflect':
+            conv_block += [nn.ReflectionPad2d(1)]
+        elif padding_type == 'replicate':
+            conv_block += [nn.ReplicationPad2d(1)]
+        else:
+            p = 1
+
+        conv_block += [GeneralConv2d(dim, dim, kernel_size=3, stride=1, padding=p)]
+
+        return nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        out_res = x + self.conv_block(x)
+        return out_res
+
+
+class ResnetBlockIns(ResnetBlock):
+    def build_conv_block(self, dim, padding_type, norm_type, use_dropout, dropout_rate):
+        conv_block = super().build_conv_block(dim, padding_type, norm_type='Ins', use_dropout, dropout_rate)
+        return conv_block
+
+
+class ResnetBlockDS(ResnetBlock):
+    def __init__(self, dim_in, dim_out, padding_type='reflect', norm_type=None, use_dropout=True, dropout_rate=0.25):
+        super(ResnetBlockDS, self).__init__(dim_out, padding_type, norm_type, use_dropout, dropout_rate)
+        self.expand_dim = nn.Conv2d(dim_in, dim_out, kernel_size=1, stride=1, padding=0) if dim_in != dim_out else None
+
+    def forward(self, x):
+        if self.expand_dim is not None:
+            x = self.expand_dim(x)
+        out_res = x + self.conv_block(x)
+        return out_res
 
 class ResnetBlock(nn.Module):
     def __init__(self, dim):
@@ -40,47 +93,6 @@ class ResnetBlock(nn.Module):
         out = x + self.conv_block(x)
         return out
 
-
-class Generator(nn.Module):
-    def __init__(self, input_nc, output_nc, n_blocks=9):
-        assert(n_blocks >= 0)
-        super(Generator, self).__init__()
-        self.input_nc = input_nc
-        self.output_nc = output_nc
-        self.n_blocks = n_blocks
-        model = [
-            nn.ReflectionPad2d(3),
-            nn.Conv2d(input_nc, ngf, kernel_size=7, padding=0, bias=True),
-            nn.InstanceNorm2d(ngf),
-            nn.ReLU(True)
-        ]
-        n_downsampling = 2
-        for i in range(n_downsampling):
-            mult = 2**i
-            model += [
-                nn.Conv2d(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1, bias=True),
-                nn.InstanceNorm2d(ngf * mult * 2),
-                nn.ReLU(True)
-            ]
-        mult = 2**n_downsampling
-        for i in range(n_blocks):
-            model += [ResnetBlock(ngf * mult)]
-        for i in range(n_downsampling):
-            mult = 2**(n_downsampling - i)
-            model += [
-                nn.ConvTranspose2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=2, padding=1, output_padding=1, bias=True),
-                nn.InstanceNorm2d(int(ngf * mult / 2)),
-                nn.ReLU(True)
-            ]
-        model += [
-            nn.ReflectionPad2d(3),
-            nn.Conv2d(ngf, output_nc, kernel_size=7, padding=0),
-            nn.Tanh()
-        ]
-        self.model = nn.Sequential(*model)
-
-    def forward(self, input):
-        return self.model(input)
 
 
 class Discriminator(nn.Module):
@@ -113,18 +125,42 @@ class Discriminator(nn.Module):
 
     def forward(self, input):
         return self.model(input)
+class DiscriminatorAux(nn.Module):
+    def __init__(self, input_nc, ndf=64, norm_layer=nn.BatchNorm2d):
+        super(DiscriminatorAux, self).__init__()
 
+        self.padding = nn.ConstantPad2d(2, 0)
+
+        self.conv1 = GeneralConv2d(input_nc, ndf, kernel_size=4, stride=2, padding=0, norm_layer=norm_layer, use_bias=False, activation=nn.LeakyReLU(0.2, True))
+        self.conv2 = GeneralConv2d(ndf, ndf * 2, kernel_size=4, stride=2, padding=0, norm_layer=norm_layer, use_bias=False, activation=nn.LeakyReLU(0.2, True))
+        self.conv3 = GeneralConv2d(ndf * 2, ndf * 4, kernel_size=4, stride=2, padding=0, norm_layer=norm_layer, use_bias=False, activation=nn.LeakyReLU(0.2, True))
+        self.conv4 = GeneralConv2d(ndf * 4, ndf * 8, kernel_size=4, stride=1, padding=0, norm_layer=norm_layer, use_bias=False, activation=nn.LeakyReLU(0.2, True))
+        self.conv5 = GeneralConv2d(ndf * 8, 2, kernel_size=4, stride=1, padding=0, use_bias=False, activation=None)
+
+    def forward(self, input):
+        x = self.padding(input)
+        x = self.conv1(x)
+        x = self.padding(x)
+        x = self.conv2(x)
+        x = self.padding(x)
+        x = self.conv3(x)
+        x = self.padding(x)
+        x = self.conv4(x)
+        x = self.padding(x)
+        x = self.conv5(x)
+        
+        return x[..., 0].unsqueeze(1), x[..., 1].unsqueeze(1)
 class DiscriminatorAux(nn.Module):
     def __init__(self, ndf, f=4, padw=2):
         super(DiscriminatorAux, self).__init__()
         self.ndf = ndf
         self.f = f
         self.padw = padw
-        self.conv1 = GeneralConv2D(1, ndf, f, 2, 0, activation_factor=0.2)
-        self.conv2 = GeneralConv2D(ndf, ndf*2, f, 2, 0, activation_factor=0.2)
-        self.conv3 = GeneralConv2D(ndf*2, ndf*4, f, 2, 0, activation_factor=0.2)
-        self.conv4 = GeneralConv2D(ndf*4, ndf*8, f, 1, 0, activation_factor=0.2)
-        self.conv5 = GeneralConv2D(ndf*8, 2, f, 1, 0, activation_factor=0, norm_type=None)
+        self.conv1 = GeneralConv2d(1, ndf, f, 2, 0, activation_factor=0.2)
+        self.conv2 = GeneralConv2d(ndf, ndf*2, f, 2, 0, activation_factor=0.2)
+        self.conv3 = GeneralConv2d(ndf*2, ndf*4, f, 2, 0, activation_factor=0.2)
+        self.conv4 = GeneralConv2d(ndf*4, ndf*8, f, 1, 0, activation_factor=0.2)
+        self.conv5 = GeneralConv2d(ndf*8, 2, f, 1, 0, activation_factor=0, norm_type=None)
 
     def forward(self, x):
         x = F.pad(x, (self.padw, self.padw, self.padw, self.padw))
@@ -140,53 +176,47 @@ class DiscriminatorAux(nn.Module):
         
         return x[..., 0].unsqueeze(1), x[..., 1].unsqueeze(1)
 
-class GeneratorResnet9blocks(nn.Module):
-    def __init__(self, ngf=64, output_nc=1, skip=False):
-        super(GeneratorResnet9blocks, self).__init__()
+class ResnetGenerator(nn.Module):
+    def __init__(self, input_nc,output_nc=1, ngf=64, norm_layer=nn.BatchNorm2d, use_dropout=False, n_blocks=9, padding_type='reflect',skip=False):
+        assert(n_blocks >= 0)
+        super(ResnetGenerator, self).__init__()
+        self.skip=skip
+        model = []
+        model += [nn.ReflectionPad2d(3),
+                  GeneralConv2dGA(input_nc, ngf, kernel_size=7, padding=0)]
 
-        # Define the reflection padding
-        self.pad = nn.ReflectionPad2d(3)
+        n_downsampling = 2
+        for i in range(n_downsampling):
+            mult = 2**i
+            model += [GeneralConv2dGA(ngf * mult, ngf * mult * 2, kernel_size=3, stride=2, padding=1),
+                      ]
 
-        # Define the downsample convolutions
-        self.conv1 = general_conv2d_ga(3, ngf, 7, 1, padding=0)
-        self.conv2 = general_conv2d_ga(ngf, ngf * 2, 3, 2, padding=1)
-        self.conv3 = general_conv2d_ga(ngf * 2, ngf * 4, 3, 2, padding=1)
+        mult = 2**n_downsampling
+        for i in range(n_blocks):
+            model += [ResnetBlockIns(ngf * mult, padding_type=padding_type, norm_type='instance', use_dropout=use_dropout, dropout_rate=0.25)]
+   
+        #upsample convolutions
+        for i in range(n_downsampling):
+            mult = 2**(n_downsampling - i)
+            model += [GeneralDeconv2d(ngf * mult, int(ngf * mult / 2), kernel_size=3, stride=2, padding=1)]
+
+
+        model += [nn.ReflectionPad2d(3),
+                  GeneralConv2dGA(ngf, output_nc, kernel_size=7, padding=0,do_relu=False),
+                  ]
+        self.model = nn.Sequential(*model)
         
-        # Define the ResNet blocks
-        self.resnet_blocks = nn.Sequential(
-            *[build_resnet_block_ins(ngf * 4, padding=1) for _ in range(9)]
-        )
-        
-        # Define the upsample convolutions
-        self.deconv1 = general_deconv2d(ngf * 4, ngf * 2, 3, 2, padding=1, output_padding=1)
-        self.deconv2 = general_deconv2d(ngf * 2, ngf, 3, 2, padding=1, output_padding=1)
-        self.conv4 = general_conv2d_ga(ngf, output_nc, 7, 1, padding=0, norm=False, activation=False)
 
-        self.skip = skip
-
-    def forward(self, inputgen, inputimg):
-        # Apply padding
-        x = self.pad(inputgen)
-
-        # Apply downsample convolutions
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-
-        # Apply ResNet blocks
-        x = self.resnet_blocks(x)
-
-        # Apply upsample convolutions
-        x = self.deconv1(x)
-        x = self.deconv2(x)
-        x = self.conv4(x)
-
-        # Apply skip connection if required
+    def forward(self,  input_gen, input_img):
+        x=self.model(input_gen)
         if self.skip:
-            return torch.tanh(inputimg + x)
+            x = torch.tanh(input_img + x)
         else:
-            return torch.tanh(x)
-# Then you can define your models like this:
+            x = torch.tanh(x)
+
+        return x
+
+
 
 class GeneratorResnet9blocks(nn.Module):
     def __init__(self, ngf=64, output_nc=1, skip=False):
@@ -235,14 +265,62 @@ class GeneratorResnet9blocks(nn.Module):
         else:
             return torch.tanh(x)
 
+class Decoder(nn.Module):
+    def __init__(self, input_nc, ngf=64, norm_layer=nn.InstanceNorm2d, use_bias=False, skip=False):
+        super(Decoder, self).__init__()
 
+        ks = 3
+        f = 7
+        padding_type = 'reflect'
+        
+        self.skip = skip
 
+        self.conv1 = GeneralConv2d(input_nc, ngf * 4, ks, stride=1, padding=ks//2, norm_layer=norm_layer, use_bias=use_bias, activation=nn.ReLU(True))
+        self.res1 = ResnetBlock(ngf * 4, padding_type=padding_type, norm_layer=norm_layer, use_bias=use_bias)
+        self.res2 = ResnetBlock(ngf * 4, padding_type=padding_type, norm_layer=norm_layer, use_bias=use_bias)
+        self.res3 = ResnetBlock(ngf * 4, padding_type=padding_type, norm_layer=norm_layer, use_bias=use_bias)
+        self.res4 = ResnetBlock(ngf * 4, padding_type=padding_type, norm_layer=norm_layer, use_bias=use_bias)
+        self.deconv1 = GeneralDeconv2d(ngf * 4, ngf * 2, ks, stride=2, padding=ks//2, output_padding=1, norm_layer=norm_layer, use_bias=use_bias, activation=nn.ReLU(True))
+        self.deconv2 = GeneralDeconv2d(ngf * 2, ngf * 2, ks, stride=2, padding=ks//2, output_padding=1, norm_layer=norm_layer, use_bias=use_bias, activation=nn.ReLU(True))
+        self.deconv3 = GeneralDeconv2d(ngf * 2, ngf, ks, stride=2, padding=ks//2, output_padding=1, norm_layer=norm_layer, use_bias=use_bias, activation=nn.ReLU(True))
+        self.conv2 = GeneralConv2d(ngf, 1, f, stride=1, padding=f//2, norm_layer=None, use_bias=use_bias, activation=None)
 
+    def forward(self, input, input_img):
+        x = self.conv1(input)
+        x = self.res1(x)
+        x = self.res2(x)
+        x = self.res3(x)
+        x = self.res4(x)
+        x = self.deconv1(x)
+        x = self.deconv2(x)
+        x = self.deconv3(x)
+        x = self.conv2(x)
+        
+        if self.skip:
+            out_gen = torch.tanh(input_img + x)
+        else:
+            out_gen = torch.tanh(x)
 
-class UNet(nn.Module):
+        return out_gen
+
+class Segmenter(nn.Module):
+    def __init__(self, input_nc, keep_rate=0.75):
+        super(Segmenter, self).__init__()
+
+        self.conv = GeneralConv2d(input_nc, 5, kernel_size=1, stride=1, padding=0, norm_layer=None, use_bias=False, activation=None)
+        self.dropout = nn.Dropout(p=1-keep_rate)
+        
+    def forward(self, input):
+        x = self.conv(input)
+        x = self.dropout(x)
+        x = F.interpolate(x, size=(256, 256), mode='bilinear', align_corners=False)
+        
+        return x
+
+class SIFA(nn.Module):
     def __init__(self, n_channels, n_classes,skip
                  , bilinear=False):
-        super(UNet, self).__init__()
+        super(SIFA, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
         self.bilinear = bilinear
@@ -252,7 +330,7 @@ class UNet(nn.Module):
         self.discriminator_B = Discriminator()
         self.discriminator_P = Discriminator()
         self.discriminator_P_ll = Discriminator()
-        self.generator_A = Generator(n_blocks=9)
+        self.generator_A = ResnetGenerator(n_blocks=9)
         self.encoder_B = Encoder()
         self.decoder_B = Decoder()
         self.segmenter_B = Segmenter()
@@ -289,17 +367,15 @@ class UNet(nn.Module):
 
         prob_fake_pool_a_is_real, prob_fake_pool_a_aux_is_real = self.discriminator_aux_A(fake_pool_a)
         prob_fake_pool_b_is_real = self.discriminator_B(fake_pool_b)
+        
+        prob_cycle_a_is_real, prob_cycle_a_aux_is_real = self.discriminator_aux_A(cycle_images_a)
 
-        # prob_fake_pool_a_is_real, prob_fake_pool_a_aux_is_real = discriminator_aux(fake_pool_a, "d_A")
-        # prob_fake_pool_b_is_real = current_discriminator(fake_pool_b, "d_B")
+        prob_pred_mask_fake_b_is_real = self.discriminator_P(pred_mask_fake_b)
+        prob_pred_mask_b_is_real = self.discriminator_P(pred_mask_b)
 
-        # prob_cycle_a_is_real, prob_cycle_a_aux_is_real = discriminator_aux(cycle_images_a, "d_A")
+        prob_pred_mask_fake_b_ll_is_real = self.discriminator_P_ll(pred_mask_fake_b_ll)
+        prob_pred_mask_b_ll_is_real = self.discriminator_P_ll(pred_mask_b_ll)
 
-        # prob_pred_mask_fake_b_is_real = current_discriminator(pred_mask_fake_b, name="d_P")
-        # prob_pred_mask_b_is_real = current_discriminator(pred_mask_b, 'd_P')
-
-        # prob_pred_mask_fake_b_ll_is_real = current_discriminator(pred_mask_fake_b_ll, name="d_P_ll")
-        # prob_pred_mask_b_ll_is_real = current_discriminator(pred_mask_b_ll, 'd_P_ll')
 
         return {
         'prob_real_a_is_real': prob_real_a_is_real,
@@ -326,39 +402,3 @@ class UNet(nn.Module):
         'prob_fake_pool_a_aux_is_real': prob_fake_pool_a_aux_is_real,
         'prob_cycle_a_aux_is_real': prob_cycle_a_aux_is_real,
     }
-
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
-        v=self.discre_start(x4,x5)
-        v=self.CRCR1(v)
-        v=self.CRCR2(v)
-        v=self.CRCR3(v)
-        v=self.CRCR4(v)
-        v = v.view(v.size(0), -1)  # flatten v
-        v = self.fc(v)
-        v = self.sigmoid(v)
-
-        return logits,v
-
-    def use_checkpointing(self):
-        self.inc = torch.utils.checkpoint(self.inc)
-        self.down1 = torch.utils.checkpoint(self.down1)
-        self.down2 = torch.utils.checkpoint(self.down2)
-        self.down3 = torch.utils.checkpoint(self.down3)
-        self.down4 = torch.utils.checkpoint(self.down4)
-        self.up1 = torch.utils.checkpoint(self.up1)
-        self.up2 = torch.utils.checkpoint(self.up2)
-        self.up3 = torch.utils.checkpoint(self.up3)
-        self.up4 = torch.utils.checkpoint(self.up4)
-        self.outc = torch.utils.checkpoint(self.outc)
-
-
-
