d04854a525c51d36a09b8c50a1aad31740586fa7
Mohammadreza Salehi <mohammadreza.salehi951@gmail.com>
2023-09-15 04:04:38 -0700

    finlize

--- unet/utils.py
+++ unet/utils.py
@@ -8,7 +8,7 @@ from skimage import measure
 import csv
 import os
 import glob
-
+import numpy as np
 # specify your path here
 
 import re
@@ -46,6 +46,7 @@ def read_csv_list(file_path):
         data = list(reader)
         
     return data[1:]
+
 
 def balanced_focal_cross_entropy_loss(
         probs:torch.Tensor, gt: torch.Tensor, focal_gamma: float = 1,
@@ -137,3 +138,57 @@ def balanced_focal_cross_entropy_loss(
                         weight=w))
 
     return torch.mean(torch.stack(losses))
+
+# from skimage import measure
+class Eval_FP():
+    def __init__(self):
+        self._threshold=.5
+        self._n_received_samples=0
+        self._c=2
+        self._tp_per_class: List[float] = np.zeros((self._c,), dtype=int)
+        self._fp_per_class: List[float] = np.zeros_like(self._tp_per_class)
+        self._fn_per_class: List[float] = np.zeros_like(self._tp_per_class)
+        self._tp_fn_threshold=0.5
+        pass
+    def eval(self,prediction,ground_truth):
+        # fill this 
+        # preds = self._
+        # read_model_seg_prediction(model_output)
+        # ground_truths = self._read_gt_seg_prediction(model_output)
+        preds=prediction
+        ground_truths=ground_truth
+        # assuming 0 as background
+        for c in range(1, self._c):
+            preds_c = (preds == c).astype(int)
+            ground_truths_c = (ground_truths == c).astype(int)
+            
+            for gt, pd in zip(ground_truths_c, preds_c):
+                self._n_received_samples += 1
+                gt_labels = measure.label(gt)
+                pd_labels = measure.label(pd)
+
+                for l in np.unique(gt_labels):
+                    if l == 0:
+                        continue
+
+                    # if ground_truth obj has enough overlap with pred, it's captured
+                    gt_obj_mask = (gt_labels == l)
+
+                    # if l has any overlap with check mask, it's captured
+                    if np.sum(
+                            (gt_obj_mask & (pd == 1))) * 1.0 / np.sum(gt_obj_mask) >= self._tp_fn_threshold:
+                        self._tp_per_class[c] += 1
+                    else:
+                        self._fn_per_class[c] += 1
+               
+                for l in np.unique(pd_labels):
+                    if l == 0:
+                        continue
+
+                    pd_obj_mask = (pd_labels == l)
+
+                    # if l has any overlap with check mask, it's captured
+                    if 1.0 * np.sum(pd_obj_mask &
+                                (gt == 1)) / \
+                            np.sum(pd_obj_mask) < self._threshold:
+                        self._fp_per_class[c] += 1
